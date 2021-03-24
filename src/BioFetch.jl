a3bbcb38a1a1ad32b017015f657c101fd488e1af
Nicholas Bauer <nicholasbauer@outlook.com>
2021-03-24 15:06:54 -0400

    Added a format argument and support for GenBank

--- src/BioFetch.jl
+++ src/BioFetch.jl
@@ -1,13 +1,21 @@
 module BioFetch
 
 using FASTX
+using GenomicAnnotations
 using BioServices.EUtils
 
+@enum Format fasta gb
 
-function fetchseq(ids::AbstractString...)
-    response = efetch(db="nuccore", id=ids, rettype="fasta", retmode="text")
-    reader = FASTA.Reader(IOBuffer(response.body))
-    records = [record for record ∈ reader]
+
+function fetchseq(ids::AbstractString...; format::Format = :fasta)
+    response = efetch(db = "nuccore", id = ids, rettype = String(format), retmode="text")
+    body = IOBuffer(response.body)
+    if format == :fasta
+        reader = FASTA.Reader(body)
+        records = [record for record ∈ reader]
+    else
+        records = readgbk(body)
+    end
     return records
 end
 
