7cea269ae213eb1bfa52add36bb6e6ff5e26d5f7
Nicholas Bauer <nicholasbauer@outlook.com>
2021-03-24 15:42:07 -0400

    Added database option and documentation

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # BioFetch.jl
  Easily fetch biological sequences from online sources
+
+BioFetch provides a higher-level interface to retrieve data from sequence databases and provide them in
+a readily manipulatable form via FASTX.jl and GenomeAnnotations.jl.
+
+Currently only supports Entrez (NCBI) Nucleotide ("nuccore") and Protein ("protein") databases, but
+the intent is to generalize it.
+
+Examples:
+```julia
+fetchseq(nuccore, "AH002844")                             # retrive one FASTA nucleotide record
+fetchseq(protein, "CAA41295.1", "NP_000176"; format = gb) # retrieve two GenBank flatfile protein records
+```
--- src/BioFetch.jl
+++ src/BioFetch.jl
@@ -6,9 +6,22 @@ using BioServices.EUtils
 
 @enum Format fasta gb
 
+@enum Database nuccore protein
 
-function fetchseq(ids::AbstractString...; format::Format = :fasta)
-    response = efetch(db = "nuccore", id = ids, rettype = String(format), retmode="text")
+"""
+    fetchseq(db::Database, ids::AbstractString...; format::Format = fasta)
+
+Fetches sequence data from a database by accession number in either FASTA format or GenBank flatfile format.
+
+Currently only supports `nuccore` and `protein` Entrez databases.
+
+```julia
+fetchseq(nuccore, "AH002844")                             # retrive one FASTA nucleotide record
+fetchseq(protein, "CAA41295.1", "NP_000176"; format = gb) # retrieve two GenBank flatfile protein records
+```
+"""
+function fetchseq(db::Database, ids::AbstractString...; format::Format = fasta)
+    response = efetch(db = String(Symbol(db)), id = ids, rettype = String(Symbol(format)), retmode="text")
     body = IOBuffer(response.body)
     if format == fasta
         reader = FASTA.Reader(body)
