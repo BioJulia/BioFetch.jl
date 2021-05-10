b0bac7f694e930dd55aaa7e6e745741f0ded994a
Nicholas Bauer <nicholasbauer@outlook.com>
2021-05-10 15:32:08 -0400

    Refined interface to infer database for NCBI

--- src/BioFetch.jl
+++ src/BioFetch.jl
@@ -6,21 +6,57 @@ using BioServices.EUtils
 
 @enum Format fasta gb
 
-@enum Database nuccore protein
-
 """
-    fetchseq(db::Database, ids::AbstractString...; format::Format = fasta)
+    fetchseq(ids::AbstractString...; format::Format = fasta)
 
 Fetches sequence data from a database by accession number in either FASTA format or GenBank flatfile format.
-
-Currently only supports `nuccore` and `protein` Entrez databases.
+Nucleotide and protein records may be mixed. Results will be returned in the order provided.
 
 ```julia
-fetchseq(nuccore, "AH002844")                             # retrive one FASTA nucleotide record
-fetchseq(protein, "CAA41295.1", "NP_000176"; format = gb) # retrieve two GenBank flatfile protein records
+fetchseq("AH002844")                             # retrive one FASTA NCBI nucleotide record
+fetchseq("CAA41295.1", "NP_000176"; format = gb) # retrieve two GenBank flatfile NCBI protein records
 ```
 """
-function fetchseq(db::Database, ids::AbstractString...; format::Format = fasta)
+function fetchseq(ids::AbstractString...; format::Format = fasta)
+    ncbinucleotides = []
+    ncbiproteins = []
+    ncbinucleotidesorder = []
+    ncbiproteinsorder = []
+    for (i, id) ∈ enumerate(ids)
+        ncbiprotein = startswith(id, r"[NX]P_|[A-Z]{3}[0-9]")
+        ncbinucleotide = startswith(id, r"[NX][CGRMW]_|[A-Z]{2}[0-9]|[A-Z]{4,6}[0-9]")
+        ncbiprotein && ncbinucleotide && throw("ambiguous identifier $id, cannot infer database")
+        ncbiprotein || ncbinucleotide || throw("could not infer database for $id")
+
+        if ncbinucleotide
+            push!(ncbinucleotides, id)
+            push!(ncbinucleotidesorder, i)
+        elseif ncbiprotein
+            push!(ncbiproteins, id)
+            push!(ncbiproteinsorder, i)
+        end
+    end
+
+    results = [fetchseq_ncbi(ncbinucleotides, "nuccore", format);
+               fetchseq_ncbi(ncbiproteins, "protein", format)]
+
+    return [results[i] for i ∈ [ncbinucleotidesorder; ncbiproteinsorder]]
+end
+
+function fetchseq_ncbi(ids::AbstractVector{<:AbstractString}, db::AbstractString, format::Format)
+    response = efetch(db = String(Symbol(db)), id = ids, rettype = String(Symbol(format)), retmode="text")
+    body = IOBuffer(response.body)
+    if format == fasta
+        reader = FASTA.Reader(body)
+        records = [record for record ∈ reader]
+    else
+        records = readgbk(body)
+    end
+    return records
+end
+
+function fetchseq_ebi(ids::AbstractString...; format::Format = fasta)
+    response = 
     response = efetch(db = String(Symbol(db)), id = ids, rettype = String(Symbol(format)), retmode="text")
     body = IOBuffer(response.body)
     if format == fasta
