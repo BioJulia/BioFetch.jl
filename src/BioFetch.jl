03230423773bebb7ac6d9dc055864ea2124b0f89
Nicholas Bauer <nicholasbauer@outlook.com>
2021-05-10 22:56:00 -0400

    Make GenBank work, refinements

--- src/BioFetch.jl
+++ src/BioFetch.jl
@@ -5,9 +5,9 @@ using GenomicAnnotations
 using BioServices.EUtils
 using BioServices.EBIProteins
 
-export fetchseq, fasta
+export fetchseq, SeqFormat, fasta, gb
 
-@enum Format fasta gb
+@enum SeqFormat fasta gb
 
 """
     fetchseq(ids::AbstractString...; format::Format = fasta)
@@ -23,7 +23,7 @@ fetchseq("AH002844")                # retrive one FASTA NCBI nucleotide record
 fetchseq("CAA41295.1", "NP_000176") # retrieve two FASTA NCBI protein records
 ```
 """
-function fetchseq(ids::AbstractString...; format::Format = fasta)
+function fetchseq(ids::AbstractString...; format::SeqFormat = fasta)
     ncbinucleotides = String[]
     ncbiproteins = String[]
     ebiuniprots = String[]
@@ -54,66 +54,60 @@ function fetchseq(ids::AbstractString...; format::Format = fasta)
     return results[order]
 end
 
-function fetchseq(id::AbstractString; format::Format = BioFetch.fasta)
+function fetchseq(id::AbstractString; format::SeqFormat = fasta)
     ebiensembl = startswith(id, r"ENS[A-Z][0-9]{11}")
     ncbiprotein = startswith(id, r"[NX]P_|[A-Z]{3}[0-9]")
     ncbinucleotide = startswith(id, r"[NX][CGRMW]_|[A-Z]{2}[0-9]|[A-Z]{4,6}[0-9]") && !ebiensembl
     ebiuniprot = startswith(id, r"[A-Z][0-9][A-Z0-9]{4}")
 
-    result = ncbinucleotide ? fetchseq_ncbi(id, "nuccore"; format) |> first :
-           ncbiprotein ? fetchseq_ncbi(id, "protein"; format) |> first :
-           ebiuniprot ? fetchseq_uniprot(id; format) :
-           ebiensembl ? fetchseq_ensembl(id; format) :
+    result = ncbinucleotide ? fetchseq_ncbi(id, "nuccore"; format) :
+           ncbiprotein ? fetchseq_ncbi(id, "protein"; format) :
+           ebiuniprot ? fetchseq_uniprot([id]; format) :
+           ebiensembl ? fetchseq_ensembl([id]; format) :
            error("could not infer database for $id")
     
-    return length(result) == 1 ? first(result) : result
+    return format == fasta && length(result) == 1 ? first(result) : result
 end
 
-function fetchseq_ncbi(ids, db::AbstractString; format::Format = fasta)
+function fetchseq_ncbi(ids, db::AbstractString; format::SeqFormat = fasta)
     isempty(ids) && return []
     response = efetch(; db, id = ids, rettype = String(Symbol(format)), retmode="text")
     body = IOBuffer(response.body)
-    if format == fasta
-        reader = FASTA.Reader(body)
-        records = [record for record ∈ reader]
-    else
-        records = readgbk(body)
-    end
+    reader = format == fasta ? FASTA.Reader(body) :
+             format == gb ? GenBank.Reader(body) :
+             nothing
+    records = [record for record ∈ reader]
     return records
 end
 
-function fetchseq_uniprot(ids::AbstractVector; format::Format = fasta)
+function fetchseq_uniprot(ids::AbstractVector; format::SeqFormat = fasta)
     isempty(ids) && return []
     records = []
     contenttype = format == fasta ? "text/x-fasta" : format == gb ? "text/flatfile" : error("unknown format")
     for id ∈ ids
         response = ebiproteins(; accession = id, contenttype)
         body = IOBuffer(response.body)
-        if format == fasta
-            reader = FASTA.Reader(body)
-            records = [record for record ∈ reader]
-        else
-            records = readgbk(body)
-        end
-        append!(records, record)
+        reader = format == fasta ? FASTA.Reader(body) :
+                 format == gb ? GenBank.Reader(body) :
+                 nothing
+        records = [record for record ∈ reader]
+        append!(records, records)
     end
     return records
 end
 
-function fetchseq_ensembl(ids::AbstractVector; format::Format = fasta)
+function fetchseq_ensembl(ids::AbstractVector; format::SeqFormat = fasta)
     isempty(ids) && return []
     records = []
     contenttype = format == fasta ? "text/x-fasta" : format == gb ? "text/flatfile" : error("unknown format")
     for id ∈ ids
         response = ebiproteins(; dbtype = "Ensembl", dbid = id, contenttype)
         body = IOBuffer(response.body)
-        if format == fasta
-            reader = FASTA.Reader(body)
-            records = [record for record ∈ reader]
-        else
-            records = readgbk(body)
-        end
-        append!(records, record)
+        reader = format == fasta ? FASTA.Reader(body) :
+                 format == gb ? GenBank.Reader(body) :
+                 nothing
+        records = [record for record ∈ reader]
+        append!(records, records)
     end
     return records
 end
