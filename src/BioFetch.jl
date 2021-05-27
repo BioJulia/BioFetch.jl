e3a12a02dcf0e9f69b519800f9776ae93e68f272
Nicholas Bauer <nicholasbauer@outlook.com>
2021-05-27 11:44:22 -0400

    Add range and strand specification (#1)

--- src/BioFetch.jl
+++ src/BioFetch.jl
@@ -10,7 +10,7 @@ export fetchseq, SeqFormat, fasta, gb
 @enum SeqFormat fasta gb
 
 """
-    fetchseq(ids::AbstractString...; format::Format = fasta)
+    fetchseq(ids::AbstractString::AbstractVector{<:AbstractString}, range::Union{Nothing, AbstractRange} = nothing, revstrand = false; format::SeqFormat = fasta)
 
 Fetches sequence data from a database by accession number in either FASTA format or GenBank flatfile format.
 Nucleotide and protein records may be mixed. Results will be returned in the order provided.
@@ -20,10 +20,14 @@ GenBank format may not work right now.
 
 ```julia
 fetchseq("AH002844")                # retrive one FASTA NCBI nucleotide record
-fetchseq("CAA41295.1", "NP_000176") # retrieve two FASTA NCBI protein records
+fetchseq(["CAA41295.1", "NP_000176"]) # retrieve two FASTA NCBI protein records
+fetchseq("NC_036893.1", 81_775_230 .+ (1:1_000_000))         # retrive a 1 Mb segment of a FASTA NCBI genomic record
+fetchseq("NC_036893.1", 81000000:81999999, true) # retrive a 1 Mb segment of a FASTA NCBI genomic record on the reverse strand
 ```
+
+`range` and `revstrand` are only valid for nucleotide queries
 """
-function fetchseq(ids::AbstractString...; format::SeqFormat = fasta)
+function fetchseq(ids::AbstractVector{<:AbstractString}, range::Union{Nothing, AbstractRange} = nothing, revstrand = false; format::SeqFormat = fasta)
     ncbinucleotides = String[]
     ncbiproteins = String[]
     ebiuniprots = String[]
@@ -32,7 +36,7 @@ function fetchseq(ids::AbstractString...; format::SeqFormat = fasta)
     for (i, id) ∈ enumerate(ids)
         ebiensembl = startswith(id, r"ENS[A-Z][0-9]{11}")
         ncbiprotein = startswith(id, r"[NX]P_|[A-Z]{3}[0-9]")
-        ncbinucleotide = startswith(id, r"[NX][CGRMW]_|[A-Z]{2}[0-9]|[A-Z]{4,6}[0-9]") && !ebiensembl
+        ncbinucleotide = startswith(id, r"[NX][CGRTMW]_|[A-Z]{2}[0-9]|[A-Z]{4,6}[0-9]") && !ebiensembl
         ebiuniprot = startswith(id, r"[A-Z][0-9][A-Z0-9]{4}")
         ncbiprotein || ncbinucleotide || ebiuniprot || ebiensembl || throw("could not infer database for $id")
 
@@ -45,7 +49,7 @@ function fetchseq(ids::AbstractString...; format::SeqFormat = fasta)
         push!(order[array], i)
     end
 
-    results = [fetchseq_ncbi(ncbinucleotides, "nuccore"; format);
+    results = [fetchseq_ncbi(ncbinucleotides, "nuccore"; format, range, revstrand);
                fetchseq_ncbi(ncbiproteins, "protein"; format);
                fetchseq_uniprot(ebiuniprots; format);
                fetchseq_ensembl(ebiensembls; format)]
@@ -54,13 +58,13 @@ function fetchseq(ids::AbstractString...; format::SeqFormat = fasta)
     return results[order]
 end
 
-function fetchseq(id::AbstractString; format::SeqFormat = fasta)
+function fetchseq(id::AbstractString, range::Union{Nothing, AbstractRange} = nothing, revstrand = false; format::SeqFormat = fasta)
     ebiensembl = startswith(id, r"ENS[A-Z][0-9]{11}")
     ncbiprotein = startswith(id, r"[NX]P_|[A-Z]{3}[0-9]")
-    ncbinucleotide = startswith(id, r"[NX][CGRMW]_|[A-Z]{2}[0-9]|[A-Z]{4,6}[0-9]") && !ebiensembl
+    ncbinucleotide = startswith(id, r"[NX][CGRTMW]_|[A-Z]{2}[0-9]|[A-Z]{4,6}[0-9]") && !ebiensembl
     ebiuniprot = startswith(id, r"[A-Z][0-9][A-Z0-9]{4}")
 
-    result = ncbinucleotide ? fetchseq_ncbi(id, "nuccore"; format) :
+    result = ncbinucleotide ? fetchseq_ncbi(id, "nuccore"; format, range, revstrand) :
            ncbiprotein ? fetchseq_ncbi(id, "protein"; format) :
            ebiuniprot ? fetchseq_uniprot([id]; format) :
            ebiensembl ? fetchseq_ensembl([id]; format) :
@@ -69,9 +73,13 @@ function fetchseq(id::AbstractString; format::SeqFormat = fasta)
     return result
 end
 
-function fetchseq_ncbi(ids, db::AbstractString; format::SeqFormat = fasta)
+function fetchseq_ncbi(ids, db::AbstractString; format::SeqFormat = fasta, range::Union{Nothing, AbstractRange} = nothing, revstrand = false)
     isempty(ids) && return []
-    response = efetch(; db, id = ids, rettype = String(Symbol(format)), retmode="text")
+    if isnothing(range)
+        response = efetch(; db, id = ids, rettype = String(Symbol(format)), retmode="text", strand = revstrand ? 2 : 1)
+    else
+        response = efetch(; db, id = ids, rettype = String(Symbol(format)), retmode="text", seq_start = first(range), seq_stop = last(range), strand = revstrand ? 2 : 1)
+    end
     body = IOBuffer(response.body)
     reader = format == fasta ? FASTA.Reader(body) :
              format == gb ? GenBank.Reader(body) :
