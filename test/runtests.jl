6dcf7d00190770ddae18b47ead2311e62e18490e
Nicholas Bauer <nicholasbauer@outlook.com>
2022-10-06 03:27:29 -0400

    Bump compat, fix test, bump version to v0.2.1

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,11 +13,11 @@ seqs = fetchseq(["CAA41295.1", "NP_000176"], format = fasta)
 @test typeof(seqs[1]) == FASTA.Record
 
 nr002726 = fetchseq("NR_002726.2", 10:20)
-nr002726rev = fetchseq("NR_002726.2", 10:20, false)
+nr002726rev = fetchseq("NR_002726.2", 10:20, true)
 @test length(nr002726) == 1
 @test typeof(nr002726[1]) == FASTA.Record
 @test length(FASTX.sequence(nr002726[1])) == 11
 @test length(FASTX.sequence(nr002726rev[1])) == 11
-@test map(x -> x == 'G' ? 'C' : x == 'C' ? 'G' : x == 'T' ? 'A' : x == 'A' ? 'T' : x, FASTX.sequence(nr002726[1])) == FASTX.sequence(nr002726rev[1])
+@test map(x -> x == 'G' ? 'C' : x == 'C' ? 'G' : x == 'T' ? 'A' : x == 'A' ? 'T' : x, FASTX.sequence(nr002726[1])) |> reverse == FASTX.sequence(nr002726rev[1])
 
 @test length(fetchseq("ENSG00000141510")) == 42
